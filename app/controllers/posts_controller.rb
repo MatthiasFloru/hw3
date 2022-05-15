6f5198a5b02ae2f6795dc8ffc392adfe80a9112e
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-05-15 14:24:18 +0000

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
         @place = Place.find_by({ "id" => params["place_id"] })
-        @post["place_id"] = @company["id"]
+        @post["place_id"] = @place["id"]
     end
 
     def create
